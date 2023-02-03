8716ad565ee35b7b2c77dabbc044632bfe934a19
Juris-Berkulis <krztfjrbhm1@jmail.com>
2023-02-03 18:29:57 +0300

    1. Add a button and create logic for it so that the user can remember the location of the cards before the start of the game; 2. Reorganize the game initialization code in the "main.js" file; 3. Rename the name of the browser tab.

--- js/main.js
+++ js/main.js
@@ -2,18 +2,75 @@ const board = document.getElementById('board');
 const score = document.getElementById('attemptNumber');
 const fail = document.getElementById('failCount');
 const startGameButton = document.getElementById('startGame');
+const rememberFieldButton = document.getElementById('rememberField');
 
 const gameManager = new GameManager(board, score, fail);
 gameManager.startGame();
 
-board.addEventListener('click', (event) => {
+const clickCard = (event) => {
     const clickedCard = event.target.connectedCard;
 
     if (clickedCard) {
         gameManager.selectCard(clickedCard);
+
+        if (!rememberFieldButton.hasAttribute('disabled')) {
+            setButtonForFlipAllCardsIsDisabled();
+
+            rememberFieldButton.removeEventListener('click', openCards);
+        }
     }
-});
+};
+
+board.addEventListener('click', clickCard);
 
 startGameButton.addEventListener('click', () => {
     gameManager.startGame();
+
+    board.addEventListener('click', clickCard);
+    setButtonForFlipAllCardsIsAbled();
 });
+
+const setButtonForFlipAllCardsIsDisabled = () => {
+    rememberFieldButton.innerHTML = 'Недоступно';
+    rememberFieldButton.style.cursor = 'default';
+    rememberFieldButton.disabled = 'true';
+};
+
+const setButtonForFlipAllCardsIsAbled = () => {
+    rememberFieldButton.innerHTML = 'Запомнить';
+    rememberFieldButton.style.cursor = 'pointer';
+    rememberFieldButton.removeAttribute('disabled');
+
+    rememberFieldButton.addEventListener('click', openCards);
+};
+
+const flipAllCards = () => {
+    for (let i=0; i < board.childNodes.length; i++) {
+        if (board.childNodes[i].connectedCard) {
+            board.childNodes[i].connectedCard.flipCard();
+        }
+    }
+};
+
+const closeCards = () => {
+    flipAllCards();
+
+    rememberFieldButton.removeEventListener('click', closeCards);
+
+    setButtonForFlipAllCardsIsDisabled();
+
+    board.addEventListener('click', clickCard);
+};
+
+const openCards = () => {
+    board.removeEventListener('click', clickCard);
+
+    flipAllCards();
+
+    rememberFieldButton.removeEventListener('click', openCards);
+    rememberFieldButton.addEventListener('click', closeCards);
+
+    rememberFieldButton.innerHTML = 'Скрыть';
+};
+
+rememberFieldButton.addEventListener('click', openCards);
