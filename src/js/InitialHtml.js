fcbb4ce0fef716d2cf26038ff50ea5491cbec0c2
Juris-Berkulis <krztfjrbhm1@jmail.com>
2023-02-11 15:10:40 +0300

    1. Create a file "InitialHtml.js" with the class "InitialHtml"; 2. Transfer the logic that inserts the initial html code into the block with the id "app" from the "main.js" file to the "InitialHtml" class of the "InitialHtml.js" file; 3. Remove import of file "styles/style.scss" from file "main.js" and add this import to file "InitialHtml.js"; 4. Import the "InitialHtml.js" file into the "main.js" file; 5. Change automatically the files in the "dist" folder.

---
+++ src/js/InitialHtml.js
@@ -0,0 +1,52 @@
+import * as styles from '../styles/style.scss';
+
+export default class InitialHtml {
+    #html = `
+        <div class="${styles.main}">
+        <div class="${styles.controlPanel}" id="controlPanel">
+            <div id="cardsCountPanel" class="${styles.cardsCountPanel}">
+                <button class="${styles.cardsCountBtn}">10</button>
+                <button class="${styles.cardsCountBtn}">20</button>
+                <button class="${styles.cardsCountBtn}">30</button>
+                <button class="${styles.cardsCountBtn}">40</button>
+            </div>
+            <button class="${styles.startGameBtn}" id="startGame">Новая игра</button>
+            <button class="${styles.startGameBtn}" id="rememberField">Запомнить</button>
+            <p>
+                <span>Попытка: </span>
+                <span id="attemptNumber"></span>
+            </p>
+            <p>
+                <span>Ошибки: </span>
+                <span id="failCount"></span>
+            </p>
+            <p id="elapsedTime"></p>
+        </div>
+        <div class="${styles.board}" id="board"></div>
+        <div class="${styles.theEnd}" id="theEnd">
+            <div class="${styles.theEndCross}">
+                <div class="${styles.theEndCrossLine} ${styles.theEndCrossLine1}"></div>
+                <div class="${styles.theEndCrossLine} ${styles.theEndCrossLine2}"></div>
+            </div>
+        </div>
+        <div class="${styles.theEndModal}" id="theEndModal">
+            <p class="${styles.theEndModalParagraph}">Вы победили!</p>
+            <p class="${styles.theEndModalParagraph}" id="theEndModalAttemptNumber"></p>
+            <p class="${styles.theEndModalParagraph}" id="theEndModalFailCount"></p>
+            <p class="${styles.theEndModalParagraph}" id="theEndModalElapsedTime"></p>
+            <div class="${styles.theEndModalCloseBtnWrapper}">
+                <button class="${styles.theEndModalCloseBtn}" id="theEndModalCloseBtn">Ok</button>
+            </div>
+        </div>
+        </div>
+    `;
+
+    constructor () {
+        this.insertHtmlInApp();
+    }
+
+    insertHtmlInApp () {
+        const app = document.getElementById('app');
+        app.innerHTML = this.#html;
+    }
+};
